a377ebeec6d19c7b5fd048a8218baf4cc14be4e7
JoJoWitness <Jordano470@gmail.com>
2023-12-27 18:33:10 -0400

    Updated the name

---
+++ NativeModules/os-system.js
@@ -0,0 +1,11 @@
+const os = require('node:os')
+
+console.log("Information of the OS")
+console.log("_____________________")
+console.log("Name: ", os.platform())
+console.log("Version: ", os.release())
+console.log("Architecture: ", os.arch())
+console.log("CPUs: ", os.cpus())
+console.log("Free memory: ", os.freemem() / 1024 / 1024)
+console.log("Total memory: ", os.totalmem() / 1024 /1024)
+console.log("Uptime: ", os.uptime() / 60 /60)
