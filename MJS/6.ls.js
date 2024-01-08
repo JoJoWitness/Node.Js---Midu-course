fd56f77110fb9008dd8eaf633f2a76772e04ddda
JoJoWitness <Jordano470@gmail.com>
2024-01-07 22:31:07 -0400

    made a small node app

---
+++ MJS/6.ls.js
@@ -0,0 +1,25 @@
+// const fs = require('node:fs')
+
+// fs.readdir('../',(err, files) =>{
+//     if(err){
+//         console.error('Error while reading the directory: ', err)
+//         return;
+//     }
+
+//     files.forEach(file =>{
+//         console.log(file)
+//     })
+// }
+// )
+
+const fs = require('node:fs/promises')
+
+fs.readdir('../')
+    .then(files =>{
+        files.forEach( file =>{
+            console.log(file)
+        })
+    })
+    .catch(err =>{
+        console.error('Error while reading the directory: ', err)
+    })
